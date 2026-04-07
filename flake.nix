4327ddf5278ac3dfb709ebccb558137ceae3e3cf
emiliopalmerini <emilio.palmerini@vivaticket.com>
2026-04-07 15:14:06 +0200

    chore: add nix dev shell with inotify-tools
    Flake dev shell provides elixir, erlang, and inotify-tools
    for Phoenix live-reload. Direnv auto-activates via .envrc.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Elven Council - MTG voting app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            elixir
+            erlang
+            inotify-tools
+          ];
+
+          shellHook = ''
+            export MIX_HOME="$PWD/.nix-mix"
+            export HEX_HOME="$PWD/.nix-hex"
+            export PATH="$MIX_HOME/bin:$MIX_HOME/escripts:$HEX_HOME/bin:$PATH"
+            export ERL_AFLAGS="-kernel shell_history enabled"
+          '';
+        };
+      });
+}
